72d4e7a0f1c64d98c9e1a52a7f0557dc83b95551
Kamal <kkjoshi.kk@gmail.com>
2023-02-06 12:48:40 +0530

    on 6-feb-initial commit

--- src/components/buttons/ButtonsComponent.test.js
+++ src/components/buttons/ButtonsComponent.test.js
@@ -1,20 +1,22 @@
 import { render, screen, cleanup } from '@testing-library/react'
 import React from 'react'
+import '@testing-library/jest-dom'
 import ButtonsComponent from './ButtonsComponent'
 
 describe('Button', () => {
   afterEach(() => {
     cleanup()
   })
+
   test('renders the Button component', () => {
     render(<ButtonsComponent />)
     const buttonElement = screen.getByRole('button')
     expect(buttonElement).toBeInTheDocument()
   })
-  test('Button Should be Disabled', () => {
-    render(<ButtonsComponent disabled="true" label="Test Button" />)
-    expect(screen.getByRole('button', { name: 'Test Button' })).toBeDisabled()
-  })
+  // test('Button Should be Disabled', () => {
+  //   render(<ButtonsComponent disabled="true" label="Test Button" />)
+  //   expect(screen.getByRole('button', { name: 'Test Button' })).toBeDisabled()
+  // })
   // test('renders correct text', () => {
   //   render(<ButtonsComponent>Submit</ButtonsComponent>)
   //   const buttonElement = screen.getByText(/submit/i)
@@ -28,3 +30,15 @@ describe('Button', () => {
   //   expect(clickHandler).toHaveBeenCalledTimes(1)
   // })
 })
+
+// import { render, fireEvent } from '@testing-library/react'
+// import React from 'react'
+// // import '@testing-library/jest-dom'
+// import Button from './Button'
+
+// test('Button component should render the children prop', () => {
+//   const text = 'Click me'
+//   const { getByText } = render(<Button>{text}</Button>)
+
+//   expect(getByText(text)).toBeInTheDocument()
+// })
